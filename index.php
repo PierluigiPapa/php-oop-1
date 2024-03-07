d35c5edd69b360c3afb60ac215a532c1d15d8fb6
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-07 11:51:27 +0100

    feat: add class and construct

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <? php
 
-
+class Movie {
+     // Variabili di istanza
+    public $title;
+    public $director;
+    public $year;
+    public $actors;
+    public $genres;
+    public $language;
+    
+     // Costruttore per inizializzare le variabili del film
+    public function __construct($title, $director,  $year, $actors, $genres, $language) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+        $this->actors = $actors;
+        $this->genres = $genres;
+        $this->language = $language;
+    }
+}
 
 
 ?>
